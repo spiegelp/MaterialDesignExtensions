7a61db36bddd3cb161df6d1eaa429c69dd4a2038
Philipp Spiegel <spiegelp@gmx.at>
2017-11-23 20:58:45 +0100

    reorganized template selector for Stepper

--- MaterialDesignExtensions/Controls/Stepper.cs
+++ MaterialDesignExtensions/Controls/Stepper.cs
@@ -452,31 +452,4 @@ namespace MaterialDesignExtensions.Controls
             Cancel = cancel;
         }
     }
-
-    internal class HorizontalStepperHeaderTemplateSelector : DataTemplateSelector
-    {
-        public override DataTemplate SelectTemplate(object item, DependencyObject container)
-        {
-            FrameworkElement element = container as FrameworkElement;
-            StepperStepViewModel step = item as StepperStepViewModel;
-
-            if (element != null && item != null)
-            {
-                if (step.IsFirstStep)
-                {
-                    return element.FindResource("firstHorizontalStepHeaderTemplate") as DataTemplate;
-                }
-                else if (step.IsLastStep)
-                {
-                    return element.FindResource("lastHorizontalStepHeaderTemplate") as DataTemplate;
-                }
-                else
-                {
-                    return element.FindResource("intermediateHorizontalStepHeaderTemplate") as DataTemplate;
-                }
-            }
-
-            return null;
-        }
-    }
 }
---
+++ MaterialDesignExtensions/TemplateSelectors/HorizontalStepperHeaderTemplateSelector.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+using MaterialDesignExtensions.Model;
+
+namespace MaterialDesignExtensions.TemplateSelectors
+{
+    internal class HorizontalStepperHeaderTemplateSelector : DataTemplateSelector
+    {
+        public HorizontalStepperHeaderTemplateSelector() { }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            FrameworkElement element = container as FrameworkElement;
+            StepperStepViewModel step = item as StepperStepViewModel;
+
+            if (element != null && step != null)
+            {
+                if (step.IsFirstStep)
+                {
+                    return element.FindResource("firstHorizontalStepHeaderTemplate") as DataTemplate;
+                }
+                else if (step.IsLastStep)
+                {
+                    return element.FindResource("lastHorizontalStepHeaderTemplate") as DataTemplate;
+                }
+                else
+                {
+                    return element.FindResource("intermediateHorizontalStepHeaderTemplate") as DataTemplate;
+                }
+            }
+
+            return null;
+        }
+    }
+}
