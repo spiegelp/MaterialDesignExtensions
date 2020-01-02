4f2b80e4b1d6f38068d2f7b3b26fab46c8ee7738
Gigas002 <gigas002@yandex.ru>
2020-01-02 16:56:12 +0300

    Bump .net framework version to 4.8

--- MaterialDesignExtensionsDemo/Properties/AssemblyInfo.cs
+++ MaterialDesignExtensionsDemo/Properties/AssemblyInfo.cs
@@ -49,5 +49,5 @@ using System.Windows;
 // Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.5")]
-[assembly: AssemblyFileVersion("1.0.0.5")]
+[assembly: AssemblyVersion("1.0.0.9")]
+[assembly: AssemblyFileVersion("1.0.0.9")]
--- MaterialDesignExtensionsTests/Properties/AssemblyInfo.cs
+++ MaterialDesignExtensionsTests/Properties/AssemblyInfo.cs
@@ -15,5 +15,5 @@ using System.Runtime.InteropServices;
 [assembly: Guid("dcf16057-4955-48f1-bd0e-28d671df59b0")]
 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.3")]
-[assembly: AssemblyFileVersion("1.0.0.3")]
+[assembly: AssemblyVersion("1.0.0.4")]
+[assembly: AssemblyFileVersion("1.0.0.4")]
